50c9d0ba3e436b569792468c362e98ad67b34557
irawanria85 <irawanria85@gmail.com>
2019-01-30 14:14:24 +0700

    Add files via upload

---
+++ google_ads_variable.js
@@ -0,0 +1,5 @@
+(function() { var ga = document.createElement('script'); ga.type =
+'text/javascript'; ga.async = true; ga.src =
+'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var
+sa = document.getElementsByTagName('script')[0];
+sa.parentNode.insertBefore(ga, sa); })();
