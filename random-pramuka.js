98d35a2df426595b6f783e14dd0a8cf528451486
irawanria85 <irawanria85@gmail.com>
2019-02-22 22:34:24 +0700

    Add files via upload

---
+++ random-pramuka.js
@@ -0,0 +1,26 @@
+var images = [],
+index = 0;
+
+images[0] = "<a href = 'https://bit.ly/lanjutin'><img src='https://1.bp.blogspot.com/-jVyYgBmFkUE/XHAUeDRf4dI/AAAAAAAAk3E/8gTWkwy8vnkc4ITHNZtI7qE8sNtcVsf0gCLcBGAs/s320/hmp_1.jpg' alt='Pramuka Indonesia'></a>";
+images[1] = "<a href = 'https://bit.ly/lanjutin'><img src='https://1.bp.blogspot.com/-MTSJkGEBnJc/XHAUh0aWcfI/AAAAAAAAk3w/SJ4RRPiqDz4anDAR_wZif6s91EKioROigCLcBGAs/s320/hmp_2.jpg' alt='Pramuka Indonesia'></a>";
+images[2] = "<a href = 'https://bit.ly/lanjutin'><img src='https://4.bp.blogspot.com/-lf-J5HaNSaI/XHAUiY3X0jI/AAAAAAAAk34/7MbilWpk7GEQJT7u2q6OQomDOdW2UDTKgCLcBGAs/s320/hmp_3.jpg' alt='Pramuka Indonesia'></a>";
+images[3] = "<a href = 'https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-mGGzuAbmAeE/XHAUiryQZtI/AAAAAAAAk38/UC9sA3oLV0EWI4uVnvuGZu1UKV7ZOjDggCLcBGAs/s320/hmp_4.jpg' alt='Pramuka Indonesia'></a>";
+images[4] = "<a href = 'https://bit.ly/lanjutin'><img src='https://1.bp.blogspot.com/-IxzS6kJYhUY/XHAUjLUL2RI/AAAAAAAAk4A/bRvjrKb7UzQwJtzonpf9h0CU6rad4g_5gCLcBGAs/s320/hmp_5.jpg' alt='Pramuka Indonesia'></a>";
+images[5] = "<a href = 'https://bit.ly/lanjutin'><img src='https://4.bp.blogspot.com/-gAcp1WuESs4/XHAUjamIlYI/AAAAAAAAk4E/i3ss_XyfXOcpaIhY45jpu9mcvtrcBq6AACLcBGAs/s320/hmp_6.jpg' alt='Pramuka Indonesia'></a>";
+images[6] = "<a href = 'https://bit.ly/lanjutin'><img src='https://1.bp.blogspot.com/-P1qhBcW2ijY/XHAUjjRu1gI/AAAAAAAAk4I/Ay71JwQRpGsGzJwVOYv85OewXSo-0F1ywCLcBGAs/s320/hmp_7.jpg' alt='Pramuka Indonesia'></a>";
+images[7] = "<a href = 'https://bit.ly/lanjutin'><img src='https://4.bp.blogspot.com/-oyBj8jmpoqw/XHAUjiJJL0I/AAAAAAAAk4M/xCAQSUsXEgIV2nx6SLzNSxrPHMoyfiZ2ACLcBGAs/s320/hmp_8.jpg' alt='Pramuka Indonesia'></a>";
+images[8] = "<a href = 'https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-gxp_CqK9Jdo/XHAUkOARRqI/AAAAAAAAk4Q/EEMvygTi4DkxIr9_EepOIAfhxNqaoX_EgCLcBGAs/s320/hmp_9.jpg' alt='Pramuka Indonesia'></a>";
+images[9] = "<a href = 'https://bit.ly/lanjutin'><img src='https://4.bp.blogspot.com/-VApWPJ62PGI/XHAUeCz4SWI/AAAAAAAAk3M/lXQEsRk9k8EBtT-xoBtetABrA3OQaUOZgCLcBGAs/s320/hmp_10.jpg' alt='Pramuka Indonesia'></a>";
+images[10] = "<a href = 'https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-vsXwDYeYZ0g/XHAUeFYgtJI/AAAAAAAAk3I/FJW8tHtiKDkrJ_45JlNj9muHdRZ5882xACLcBGAs/s320/hmp_11.jpg' alt='Pramuka Indonesia'></a>";
+images[11] = "<a href = 'https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-F3iqwaPuUqY/XHAUe3PX_zI/AAAAAAAAk3Q/WJoBnElXMOouZtQw8IsU_o0C95zZOAoJgCLcBGAs/s320/hmp_12.jpg' alt='Pramuka Indonesia'></a>";
+images[12] = "<a href = 'https://bit.ly/lanjutin'><img src='https://4.bp.blogspot.com/-YgNN9alLbKQ/XHAUfsWO97I/AAAAAAAAk3U/Gtfpe3DAyccpnz3WVv0sZmUly_Y9U1AeQCLcBGAs/s320/hmp_13.jpg' alt='Pramuka Indonesia'></a>";
+images[13] = "<a href = 'https://bit.ly/lanjutin'><img src='https://1.bp.blogspot.com/-mi-LpoYo1IY/XHAUf6jl2RI/AAAAAAAAk3Y/FTQdAesN2AIszuI_rifRWWnnRPrK1kWSgCLcBGAs/s320/hmp_14.jpg' alt='Pramuka Indonesia'></a>";
+images[14] = "<a href = 'https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-ee_2pYSPnsU/XHAUgPfMdxI/AAAAAAAAk3c/7e_T98QxaLQMWje1RqR8Oe9hgdIqLzlVQCLcBGAs/s320/hmp_15.jpg' alt='Pramuka Indonesia'></a>";
+images[15] = "<a href = 'https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-U-Iuzuivz8c/XHAUgW-7efI/AAAAAAAAk3g/RQWZidXw5wgWtwu_ESCUXfaIyQOJB4XTACLcBGAs/s320/hmp_16.jpg' alt='Pramuka Indonesia'></a>";
+images[16] = "<a href = 'https://bit.ly/lanjutin'><img src='https://3.bp.blogspot.com/-N8areweY7zY/XHAUgzPUFXI/AAAAAAAAk3k/XuxHuNgyG2sAo7cYd67dbTpUBOeOzOn0ACLcBGAs/s320/hmp_17.jpg' alt='Pramuka Indonesia'></a>";
+images[17] = "<a href = 'https://bit.ly/lanjutin'><img src='https://4.bp.blogspot.com/-9zoqilNSzDc/XHAUg3sNfWI/AAAAAAAAk3o/Fi7H5G6UtWENzIUjOeJh8L9z4EuHowMgwCLcBGAs/s320/hmp_18.jpg' alt='Pramuka Indonesia'></a>";
+images[18] = "<a href = 'https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-cSpnahWXj4M/XHAUhDyiT0I/AAAAAAAAk3s/RcvC00RPhck-Esu3-_DuFoMojPVDQ7AfgCLcBGAs/s320/hmp_19.jpg' alt='Pramuka Indonesia'></a>";
+images[19] = "<a href = 'https://bit.ly/lanjutin'><img src='https://4.bp.blogspot.com/-cejYgsC8FXw/XHAUibLyNzI/AAAAAAAAk30/1Sk4H2_eyE82YCj-GaxdLSuk0BQqLhcIQCLcBGAs/s320/hmp_20.jpg' alt='Pramuka Indonesia'></a>";
+
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
