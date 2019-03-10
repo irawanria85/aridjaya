924a6315f40064707d52e9efcb88e04042a13806
irawanria85 <irawanria85@gmail.com>
2019-03-10 13:45:59 +0700

    Update gambar-hot.js

--- gambar-hot.js
+++ gambar-hot.js
@@ -4,6 +4,8 @@ var images = [],
 images[0] = "<a href ='https://bit.ly/bablasin'><img src='https://1.bp.blogspot.com/-r8pphPlI-yA/XISpsGOiC4I/AAAAAAAAqlY/JVYE1x55SxMqOauXIQrDr3fTwqolPIzhwCPcBGAYYCw/s640/banner-bokep.gif' style='width:100%!important' ></a>";
 images[1] = "<a href ='https://bit.ly/lanjutin'><img src='https://1.bp.blogspot.com/-AKFM0o78P9g/XISpq1NEY7I/AAAAAAAAqlU/M9-Y6N_cgYwnA5gcjU2X447f7XMCuRCeQCPcBGAYYCw/s640/banner-vimax.gif' style='width:100%!important' ></a>";
 images[2] = "<a href ='https://bit.ly/gotowait'><img src='https://2.bp.blogspot.com/-UHhy9L8JPsU/XISp0hFn3GI/AAAAAAAAqlI/0pktb9anKOwuvAmGJlXDWbCthfChYGNBgCPcBGAYYCw/s640/nete1.jpg' style='width:100%!important' ></a>";
+images[3] = "<a href ='https://bit.ly/bablasin'><img src='https://1.bp.blogspot.com/-u70oGsM7k9s/XISxrXqJEcI/AAAAAAAAql0/XtZ3qz-3RT8EI76GNvxQEF3zNqmPguRcgCK4BGAYYCw/s640/body-sex.jpg' style='width:100%!important' ></a>";
+images[4] = "<a href ='https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-rzGY_qEzJaw/XISyL1rkeCI/AAAAAAAAqmE/mkaiv8fGvXk4PPOyRbMa8-Nq04kjtfWDwCLcBGAs/s640/img_200x200.gif' style='width:100%!important' ></a>";
 
 index = Math.floor(Math.random() * images.length);
 document.write(images[index]);
