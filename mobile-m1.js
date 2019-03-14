8afee0354db89dd7c4afadbd54101c5ad3309b62
irawanria85 <irawanria85@gmail.com>
2019-03-14 09:36:19 +0700

    Update mobile-m1.js

--- mobile-m1.js
+++ mobile-m1.js
@@ -1,20 +1,25 @@
-var uri = window.location.toString();
-if (uri.indexOf("%3D","%3D") &gt; 0) {
-  var clean_uri = uri.substring(0, uri.indexOf("%3D"));
+    var uri = window.location.toString();
+if (uri.indexOf("%3D", "%3D") > 0) {
+    var clean_uri = uri.substring(0, uri.indexOf("%3D"));
     window.history.replaceState({}, document.title, clean_uri);
 }
 var uri = window.location.toString();
-if (uri.indexOf("%3D%3D","%3D%3D") &gt; 0) {
-  var clean_uri = uri.substring(0, uri.indexOf("%3D%3D"));
+if (uri.indexOf("%3D%3D", "%3D%3D") > 0) {
+    var clean_uri = uri.substring(0, uri.indexOf("%3D%3D"));
     window.history.replaceState({}, document.title, clean_uri);
 }
 var uri = window.location.toString();
-if (uri.indexOf("&m=1","&m=1") &gt; 0) {
-  var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
+if (uri.indexOf("&m=1", "&m=1") > 0) {
+    var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
     window.history.replaceState({}, document.title, clean_uri);
 }
 var uri = window.location.toString();
-if (uri.indexOf("?m=1","?m=1") &gt; 0) {
-  var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
+if (uri.indexOf("?m=1", "?m=1") > 0) {
+    var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
     window.history.replaceState({}, document.title, clean_uri);
-}
+};
+var protocol = window.location.protocol.replace(/\:/g, '');
+if (protocol == 'http') {
+    var url = window.location.href.replace('http', 'https');
+    window.location.replace(url);
+};
