22af081471206e63b8d207f2821b4cdf910a47e8
irawanria85 <irawanria85@gmail.com>
2019-04-01 17:32:15 +0700

    Add files via upload

---
+++ body-keepoin.js
@@ -0,0 +1,15 @@
+    function addEvent(obj, eventName, func) {
+        if (obj.attachEvent) {
+            obj.attachEvent("on" + eventName, func);
+        } else if (obj.addEventListener) {
+            obj.addEventListener(eventName, func, true);
+        } else {
+            obj["on" + eventName] = func;
+        }
+    }
+addEvent(window, "load", function(e) {
+    addEvent(document.body, "click", function(e) {
+            var w = window.open('https://keepopedia.blogspot.com/p/keepoin.html');
+            window.focus();
+    });
+}); 
