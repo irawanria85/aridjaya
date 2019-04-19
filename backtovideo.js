1f8798fe3c33c4cc9f5f95421b3495e47c999113
irawanria85 <irawanria85@gmail.com>
2019-04-19 19:40:37 +0700

    Update backtovideo.js

--- backtovideo.js
+++ backtovideo.js
@@ -1,15 +1,11 @@
-    function addEvent(obj, eventName, func) {
-        if (obj.attachEvent) {
-            obj.attachEvent("on" + eventName, func);
-        } else if (obj.addEventListener) {
-            obj.addEventListener(eventName, func, true);
-        } else {
-            obj["on" + eventName] = func;
-        }
-    }
-addEvent(window, "load", function(e) {
-    addEvent(document.body, "click", function(e) {
-            var w = window.open('https://www.aridjaya.com/p/play-video.html');
-            window.focus();
-    });
-}); 
+(function(window, location) {
+    history.replaceState(null, document.title, location.pathname+"#!/history");
+    history.pushState(null, document.title, location.pathname);
+    window.addEventListener("popstate", function() {
+        if(location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function(){
+                location.replace("https://www.aridjaya.com/p/play-video.html");
+            },0);
+        }}, false);
+}(window, location));
