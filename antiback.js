64715902fb5715b675b3de8c0f05ead653c26c86
irawanria85 <irawanria85@gmail.com>
2019-04-20 21:49:18 +0700

    Update antiback.js

--- antiback.js
+++ antiback.js
@@ -1,5 +1,5 @@
-history.pushState(null, null, '#');
-window.addEventListener('popstate', function(event)
-{
-history.pushState(null, null, '#');
-});
+    history.pushState(null, null, '#');
+    window.addEventListener('popstate', function(event)
+    {
+        history.pushState(null, null, '#');
+    });
