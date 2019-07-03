19d70af3858fcbdeef0e6ba0f018974b45cc7e02
irawanria85 <irawanria85@gmail.com>
2019-07-03 16:05:23 +0700

    Update and rename backtovideo.js to backtokeepoin.js

--- backtokeepoin.js
+++ backtokeepoin.js
@@ -5,7 +5,7 @@
         if(location.hash === "#!/history") {
             history.replaceState(null, document.title, location.pathname);
             setTimeout(function(){
-                location.replace("https://aridjaya.blogspot.com/p/play-video.html");
+                location.replace("https://keepopedia.blogspot.com/p/keepoin.html");
             },0);
         }}, false);
 }(window, location));
