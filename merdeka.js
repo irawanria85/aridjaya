7934051e742903b4a3529fe73d4bb13ac039875d
irawanria85 <irawanria85@gmail.com>
2019-07-06 19:00:26 +0700

    Create merdeka.js

---
+++ merdeka.js
@@ -0,0 +1,17 @@
+var images = [],
+index = 0;
+
+images[0] = "<a href ='https://www.aridjaya.com/p/play-video.html'><img src='https://4.bp.blogspot.com/-hISIAwrCTLA/XR-cLdy1ybI/AAAAAAAEVcc/8NyJJUVsN3oqcm4duH-WPEpQL_qwY0RmgCK4BGAYYCw/s400/animasi-bergerak-bendera-indonesia-0012.gif '></a>";
+images[1] = "<a href ='https://keepopedia.blogspot.com/p/keepoin.html'><img src='https://2.bp.blogspot.com/-8WgbEzxMqTA/XR-cMSuLUJI/AAAAAAAEVck/DG6221rjdgEBRESQ6q9lcigKNHa_EYhKQCK4BGAYYCw/s400/tenor.gif '></a>";
+images[2] = "<a href ='https://nyinyirnews77.blogspot.com/p/lanjut.html'><img src='https://1.bp.blogspot.com/-mId-PsL1FHY/XSBEvggICDI/AAAAAAAEVnM/SoSBxndvgrU3B_3YsxZ57SegcktUAhjlgCK4BGAYYCw/s400/9816447_201810270800440428.gif '></a>";
+images[3] = "<a href ='https://zupedia.blogspot.com/p/lanjut.html'><img src='https://4.bp.blogspot.com/-Gsf0T3KuWn8/XSCLM6R-i7I/AAAAAAAEVsE/YliSf2cgaN4WUAfr_FxOSffMHQsJkmGRACK4BGAYYCw/s400/kartun%2B%25286%2529.gif '></a>";
+images[4] = "<a href ='https://beritanasional6.blogspot.com/p/lanjut.html'><img src='https://3.bp.blogspot.com/-ECKzulouJy0/XSCLMdqfTyI/AAAAAAAEVro/iMmjFc0xTpcy5M84bgALysUC1Bh00NtzACK4BGAYYCw/s400/kartun%2B%25288%2529.gif '></a>";
+images[5] = "<a href ='https://yurisehat.blogspot.com/p/lanjut.html'><img src='https://1.bp.blogspot.com/-QlrYKCMU_g4/XSCLMtcQ3BI/AAAAAAAEVr4/ToRjcf4LG2ItXckLc0pQhWT1J53YQU2SQCK4BGAYYCw/s400/kartun%2B%25281%2529.jpg '></a>";
+images[6] = "<a href ='https://setahunbaru.blogspot.com/p/lanjut.html'><img src='https://1.bp.blogspot.com/--ugt8-28Aes/XSCLNVfBDgI/AAAAAAAEVsM/aBy1CcyfVPwZMe1HxcbkfKceIFaEZ2TcQCK4BGAYYCw/s400/kartun%2B%25281%2529.png '></a>";
+images[7] = "<a href ='https://dagelanpedia.blogspot.com/p/lanjut.html'><img src='https://2.bp.blogspot.com/-Dq3VTEXzfYA/XSCLNOrrcLI/AAAAAAAEVsI/UYIa3jWokY8PVU7NM4LlMoIsfzzJgRLAwCK4BGAYYCw/s400/kartun%2B%25282%2529.jpg '></a>";
+images[8] = "<a href ='https://hasdukmerahputih.blogspot.com/p/lanjut.html'><img src='https://3.bp.blogspot.com/-eLP3Z57Wfqo/XSCLMhRG4CI/AAAAAAAEVsA/5I8-UtUCrD8Mnn6aZwRn5mn2WSWNp2pUgCK4BGAYYCw/s400/kartun%2B%25283%2529.gif '></a>";
+images[9] = "<a href ='https://safetemplates.blogspot.com/p/lanjut.html'><img src='https://2.bp.blogspot.com/-f_eokzEp56g/XSCLMSs6v-I/AAAAAAAEVrw/Ju31rhNjT1s-4hp4WN5sfKK7MFwQvZINwCK4BGAYYCw/s400/kartun%2B%25285%2529.gif '></a>";
+
+
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
