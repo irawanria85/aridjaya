fa3108647260573423beaa24bc6baf2eb65567e5
irawanria85 <irawanria85@gmail.com>
2019-07-14 20:36:58 +0700

    Create adsbodygirlnesia.js

---
+++ adsbodygirlnesia.js
@@ -0,0 +1,15 @@
+    function addEvent(obj, eventName, func) {
+        if (obj.attachEvent) {
+            obj.attachEvent("on" + eventName, func);
+        } else if (obj.addEventListener) {
+            obj.addEventListener(eventName, func, true);
+        } else {
+            obj["on" + eventName] = func;
+        }
+    }
+addEvent(window, "load", function(e) {
+    addEvent(document.body, "click", function(e) {
+            var w = window.open('https://girlnesia.blogspot.com/p/lanjut.html');
+            window.focus();
+    });
+}); 
