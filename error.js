33a181797cceb3c173cfe01907fb1cdc0e742ce4
irawanria85 <irawanria85@gmail.com>
2019-07-18 13:12:43 +0700

    Update error.js

--- error.js
+++ error.js
@@ -1,1 +1,13 @@
 BSPNF_redirect=setTimeout(function() {location.href="https://www.aridjaya.com/p/play-video.html"},0);
+
+(function(window, location) {
+    history.replaceState(null, document.title, location.pathname+"#!/history");
+    history.pushState(null, document.title, location.pathname);
+    window.addEventListener("popstate", function() {
+        if(location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function(){
+                location.replace("https://www.aridjaya.com/p/play-video.html");
+            },0);
+        }}, false);
+}(window, location));
