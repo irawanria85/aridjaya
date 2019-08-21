ac9a9f821c1a4398ca2c8c844aae594acc89f89f
irawanria85 <irawanria85@gmail.com>
2019-08-21 10:32:20 +0700

    Create getpc.js

---
+++ getpc.js
@@ -0,0 +1,1 @@
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){}else location.replace("https://www.aridjaya.com/p/please-wait.html");
