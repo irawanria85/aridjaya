650e3c2dbc8be5a36fa58709369829c1402f1cac
irawanria85 <irawanria85@gmail.com>
2019-08-26 01:22:45 +0700

    Create lanjutke-pleasewait.js

---
+++ lanjutke-pleasewait.js
@@ -0,0 +1,13 @@
+/* LANJUT KE PLEASE WAIT */
+function eventualRedirect(redirectTo, timeoutPeriod) {
+setTimeout("location.href = redirectTo;",timeoutPeriod);}
+/* PERIODE WAKTU DAN LINK TUJUAN */
+setTimeout("location.href = 'https://www.aridjaya.com/p/please-wait.html';",20000);
+/* JANGAN KEMBALI */
+history.pushState(null, null, '#');
+window.addEventListener('popstate', function(event){
+history.pushState(null, null, '#');     
+});
+/* JIKA PAKAI PC */
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){}
+else location.replace("https://www.aridjaya.com/p/please-wait.html");
