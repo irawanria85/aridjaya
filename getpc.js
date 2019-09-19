4dbf91e27384cbe3230020e38ee6fc0dea5cb883
irawanria85 <irawanria85@gmail.com>
2019-09-20 04:05:52 +0700

    Update getpc.js

--- getpc.js
+++ getpc.js
@@ -1,1 +1,3 @@
-if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){}else location.replace("https://www.aridjaya.com/p/please-wait.html");
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){
+}
+else location.replace("/p/lanjut.html");
