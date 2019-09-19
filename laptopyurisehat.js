e172ee39122fae4f5444a8877ebbe5661c84dd8c
irawanria85 <irawanria85@gmail.com>
2019-09-20 04:12:07 +0700

    Create laptopyurisehat.js

---
+++ laptopyurisehat.js
@@ -0,0 +1,3 @@
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){
+}
+else location.replace("https://yurisehat.blogspot.com/p/lanjut.html");
