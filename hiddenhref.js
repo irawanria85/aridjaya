c18751691359429e04fc7e3f17cace6c652b7180
irawanria85 <irawanria85@gmail.com>
2019-09-21 11:17:59 +0700

    Update hiddenhref.js

--- hiddenhref.js
+++ hiddenhref.js
@@ -9,3 +9,11 @@ $(function() {
         window.open(url, '_blank')
     })
 });
+(function(window,location){
+    history.replaceState(null,document.title,location.pathname+"#!/history");
+    history.pushState(null,document.title,location.pathname);
+    window.addEventListener("popstate",function(){
+        if(location.hash==="#!/history"){
+        history.replaceState(null,document.title,location.pathname);
+            setTimeout(function(){location.replace("/p/please-wait.html");},0);}},false);}(
+   window,location));
