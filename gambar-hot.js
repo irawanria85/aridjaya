a2f4a9223e3959615c7e43928bde6cfd5d2dcefd
irawanria85 <irawanria85@gmail.com>
2019-11-02 04:18:23 +0700

    Update gambar-hot.js

--- gambar-hot.js
+++ gambar-hot.js
@@ -1,11 +1,16 @@
 var images = [],
     index = 0;
 	
-images[0] = "<a href ='https://bit.ly/bablasin'><img src='https://1.bp.blogspot.com/-r8pphPlI-yA/XISpsGOiC4I/AAAAAAAAqlY/JVYE1x55SxMqOauXIQrDr3fTwqolPIzhwCPcBGAYYCw/s640/banner-bokep.gif' style='width:100%!important' ></a>";
-images[1] = "<a href ='https://bit.ly/lanjutin'><img src='https://1.bp.blogspot.com/-AKFM0o78P9g/XISpq1NEY7I/AAAAAAAAqlU/M9-Y6N_cgYwnA5gcjU2X447f7XMCuRCeQCPcBGAYYCw/s640/banner-vimax.gif' style='width:100%!important' ></a>";
-images[2] = "<a href ='https://bit.ly/gotowait'><img src='https://2.bp.blogspot.com/-UHhy9L8JPsU/XISp0hFn3GI/AAAAAAAAqlI/0pktb9anKOwuvAmGJlXDWbCthfChYGNBgCPcBGAYYCw/s640/nete1.jpg' style='width:100%!important' ></a>";
-images[3] = "<a href ='https://bit.ly/bablasin'><img src='https://1.bp.blogspot.com/-u70oGsM7k9s/XISxrXqJEcI/AAAAAAAAql0/XtZ3qz-3RT8EI76GNvxQEF3zNqmPguRcgCK4BGAYYCw/s640/body-sex.jpg' style='width:100%!important' ></a>";
-images[4] = "<a href ='https://bit.ly/lanjutin'><img src='https://2.bp.blogspot.com/-rzGY_qEzJaw/XISyL1rkeCI/AAAAAAAAqmE/mkaiv8fGvXk4PPOyRbMa8-Nq04kjtfWDwCLcBGAs/s640/img_200x200.gif' style='width:100%!important' ></a>";
+images[0] = "<img src='https://1.bp.blogspot.com/-0L3GqdQk2B8/XbygLfHHtfI/AAAAAAAEdi0/Pzhc_oLZs1MSfQNXb6dEO6W9HOjsrQkRQCLcBGAsYHQ/s1600/nona_3.gif'/>";
+images[1] = "<img src='https://1.bp.blogspot.com/-AkZs9SXR1bI/XbygGmn7IjI/AAAAAAAEdiw/_54U_slZM4ElToHftlu-Pvpx8x6v2t7KQCLcBGAsYHQ/s1600/nona_5.gif'/>";
+images[2] = "<img src='https://1.bp.blogspot.com/-DR_nlpedeNw/Xbyf7mi-GwI/AAAAAAAEdik/9D7VjpEawZUapw1G6tJ4tyogQgHg_P-2gCLcBGAsYHQ/s1600/nona_1.gif'/>";
+images[3] = "<img src='https://1.bp.blogspot.com/-lqVyI8h5wv4/Xbyf8T87owI/AAAAAAAEdio/rHpCWq7Z3BUjDH_gDZhUvvZQV994qrgtQCLcBGAsYHQ/s1600/nona_2.gif'/>";
+images[4] = "<img src='https://1.bp.blogspot.com/-m61c8k2Oku4/XbygUU5l93I/AAAAAAAEdjM/SViLnLyN9l0G9pbuXr3hPC8p6sEkgeGyQCLcBGAsYHQ/s1600/nona_9.gif'/>";
+images[5] = "<img src='https://1.bp.blogspot.com/-NTgstLMUEBc/XbygVWpT4EI/AAAAAAAEdjQ/MPLjz3EJ3akazlxSXx4ldKrhflBGARPngCLcBGAsYHQ/s1600/nona_8.gif'/>";
+images[6] = "<img src='https://1.bp.blogspot.com/-pXEWiaA74Ak/XbygMizCqDI/AAAAAAAEdi4/4pKrXhlATkoEUxFc6IPX_LVi5n2aoS06ACLcBGAsYHQ/s1600/nona_6.gif'/>";
+images[7] = "<img src='https://1.bp.blogspot.com/-ueJYmoerDkA/Xbyf80OWJ3I/AAAAAAAEdis/8CVt-DwdgZ0pq9ltus7bcE9WtobS-YjIgCLcBGAsYHQ/s1600/nona_10.gif'/>";
+images[8] = "<img src='https://1.bp.blogspot.com/-zFD5JuOQ0QI/XbygR3wMyaI/AAAAAAAEdjI/9D7Ar2Sih-EsHqX5Li2wqbfT0Lk1yRZ4QCLcBGAsYHQ/s1600/nona_7.gif'/>";
+images[9] = "<img src='https://1.bp.blogspot.com/-zL_npSDL13Q/XbygNBiu4BI/AAAAAAAEdi8/roEV1FkiW1oCJtH9wbHUhUg14KqMn71qgCLcBGAsYHQ/s1600/nona_4.gif'/>";
 
 index = Math.floor(Math.random() * images.length);
 document.write(images[index]);
