1ebe712acdb91ded97f465e6e96b42bd9ca3c1bb
irawanria85 <irawanria85@gmail.com>
2020-04-12 03:57:00 +0700

    Create ramadhanpeople.js

---
+++ ramadhanpeople.js
@@ -0,0 +1,13 @@
+var images = [],
+index = 0;
+
+images[0] = "<a href='//bebastagihan.blogspot.com/'><img src='https://1.bp.blogspot.com/-xsXwqXbQTls/XpItMoUs1II/AAAAAAAEwLk/NJGyMTLp_cgW8eLVKkVTRUt7MsmgJTl9wCLcBGAsYHQ/s320/1.gif'/></a>";
+images[1] = "<a href='//blangkonkuota.blogspot.com/'><img src='https://1.bp.blogspot.com/-zuugc0v0SrI/XpItMlECf2I/AAAAAAAEwLg/KUApYXzZ3OMHXb5CKRmw3ffekDLUfcrxgCLcBGAsYHQ/s320/2.gif'/></a>";
+images[2] = "<a href='//lapakrecehan.blogspot.com/'><img src='https://1.bp.blogspot.com/-XWJ51aAyISg/XpItMjz2RzI/AAAAAAAEwLc/hClYW5i8o5oIjpZHJnJm0RmzdH-fNViKgCLcBGAsYHQ/s320/3.gif'/></a>";
+images[3] = "<a href='//saldoppob.blogspot.com/'><img src='https://1.bp.blogspot.com/-OUMQUxAJvIc/XpItNqoJVZI/AAAAAAAEwLo/LpPbWeycdakW7rlt7rvIBDWgbDktMEy9QCLcBGAsYHQ/s320/4.gif'/></a>";
+images[4] = "<a href='//solawatpulsa.blogspot.com/'><img src='https://1.bp.blogspot.com/--gYDPCNuCcA/XpItN2_g94I/AAAAAAAEwLw/1h1jucJqqww-6mU6PwJU2avU7LloAxJ3wCLcBGAsYHQ/s320/5.gif'/></a>";
+images[5] = "<a href='//tawontube.blogspot.com/'><img src='https://1.bp.blogspot.com/-yhOFmXtWtPU/XpItOGqli3I/AAAAAAAEwLs/BJJ15Vj9pTkc7yy5XWLLLRFm7EQNG5StACLcBGAsYHQ/s1600/6.gif'/></a>";
+images[6] = "<a href='//tempatagenpulsa.blogspot.com/'><img src='https://1.bp.blogspot.com/-auzb1RrYd2o/XpItO5wM1II/AAAAAAAEwL0/YJP0DIbkyxwx9aJ_-DI0r9REL9foMOa_wCLcBGAsYHQ/s320/7.gif'/></a>";
+
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
